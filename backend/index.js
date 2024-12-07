4d76a3eb5cbe4869556f9c231e28038021f522c7
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-12-07 09:35:03 +0530

    new changes

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const maintenanceRouter = require('./routes/maintenanceRouter');
 const assignRouter = require('./routes/assignRouter');
 
 
-const PORT=5000;
+const PORT=process.env.PORT||5000;
 app.use(express.json())
 app.use(cors())
 
