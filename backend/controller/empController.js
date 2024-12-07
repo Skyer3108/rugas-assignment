2caae19594394cd2fad48f625291fc594a67ca78
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-12-07 09:49:34 +0530

    bcrypt-changes

--- backend/controller/empController.js
+++ backend/controller/empController.js
@@ -1,6 +1,6 @@
 const EmployeeSchema=require('../schema/Employee')
 const jwt=require('jsonwebtoken')
-const bcrypt=require('bcrypt')
+const bcrypt=require('bcryptjs')
 const validator = require('validator');
 
 
